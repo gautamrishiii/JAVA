8fbe2d64320c439335944499a6b16decb70cdb38
Gautam Rishi <gautamyadav73240@gmail.com>
2026-09-20 12:15:37 +0530

    Java

--- shorthand.java
+++ shorthand.java
@@ -260,38 +260,218 @@
 //     }
 // }
 
+// import java.util.Scanner;
+// public class shorthand{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter the length of the array");
+//         int size=sc.nextInt();
+//         int[] arr=new int[size];
+//         for(int i=0;i<size;i++){
+//             System.out.print("enter the element at index = "+i +" ");
+//             arr[i]=sc.nextInt();
+//         }
+//         for(int i=0;i<arr.length;i++){
+//             System.out.print(arr[i]);
+//             System.out.print(" ");
+//         }
+//         int total=sum(arr);
+//         System.out.println("the totla sum is = "+total);
+//         double avg=avg(arr);
+//         System.out.println("the avg is = "+avg);
+        
+//     }
+//     public static int sum(int[] arr){
+//         int sum=0;
+//         for(int i=0;i<arr.length;i++){
+//             sum +=arr[i];
+//         }
+//         return sum;
+//     }
+//     public static double avg(int[] arr){
+       
+//         double avg=(double) sum(arr)/arr.length;
+//         return avg;
+//     }
+    
+// }
+
+// import java.util.Scanner;
+// public class shorthand{
+//     public static void main(){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter the size of the array");
+//         int size=sc.nextInt();
+//         int[] arr=new int[size];
+//         int i=0;
+//         while(i<arr.length){
+//             System.out.println("print the element at index "+i+" ");
+//             arr[i]=sc.nextInt();
+//             i++;
+
+//         }
+//         for(int j=0;j<arr.length;j++){
+//             System.out.print(arr[i]+" ");
+//         }
+        
+//         System.out.println("enter the element you want to searach");
+//         int search=sc.nextInt();
+//         if(search==arr[i]){
+//             System.out.println("the element is found at index"+arr[i]);  
+//         }
+//         // int ele=occurance(arr);
+//         // System.out.println(ele);
+        
+//     }
+//     // public static int occurance(int[] arr){
+//     //     Scanner sc = new Scanner(System.in);
+//     //     System.out.println("enter the element you want to searach");
+//     //     int search=sc.nextInt();
+//     //     if(search==arr[i]){
+//     //         System.out.println("the element is found at index"+arr[i]);  
+//     //     }
+//     //     return 0;
+
+//     // }
+// }
+// import java.util.Scanner;
+// public class shorthand{
+//     public static void main(){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter the size of the array");
+//         int size=sc.nextInt();
+//         int[] arr=new int[size];
+       
+//         for(int i=0;i<arr.length;i++){
+//             System.out.println("print the element at index "+i);
+//             arr[i]=sc.nextInt();
+
+//         }
+//         for(int i=0;i<arr.length;i++){
+//             System.out.print(arr[i]+" ");
+//         }
+//         System.out.println("enter the element you want to searach");
+//         int search=sc.nextInt();
+//         for(int i=0;i<arr.length;i++){
+//             if(arr[i]==search){
+//             System.out.println("the element is found at index"+arr[i]);  
+//             break;
+//         }else{
+//             System.out.println("element not found");
+
+//         }
+        
+//         }
+      
+//     }
+
+// }
+
+// import java.util.Scanner;
+// public class shorthand{
+//     public static void main(){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter the size of the array");
+//         int size=sc.nextInt();
+//         int[] arr=new int[size];
+       
+//         for(int i=0;i<arr.length;i++){
+//             System.out.println("print the element at index "+i);
+//             arr[i]=sc.nextInt();
+
+//         }
+//         for(int i=0;i<arr.length;i++){
+//             System.out.print(arr[i]+" ");
+//         }
+//         System.out.println("enter the element you want to searach");
+//         int search=sc.nextInt();
+//         for(int i=0;i<arr.length;i++){
+//             if(arr[i]==search){
+//             System.out.println("the element is found at index"+arr[i]);  
+//             break;
+//         }else{
+//             System.out.println("element not found");
+
+//         }
+        
+//         }
+      
+//     }
+
+// }
+// import java.util.Scanner;
+// public class shorthand{
+//     public static void main(){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter the size of the array");
+//         int size=sc.nextInt();
+//         int[] arr=new int[size];
+       
+//         for(int i=0;i<arr.length;i++){
+//             System.out.println("print the element at index "+i);
+//             arr[i]=sc.nextInt();
+
+//         }
+//         for(int i=0;i<arr.length;i++){
+//             System.out.print(arr[i]+" ");
+//         }
+//         System.out.println("enter the element you want to searach the occurence");
+//         int search=sc.nextInt();
+//         int occurance=0;
+//         for(int i=0;i<arr.length;i++){
+//             if(arr[i]==search){
+//             occurance++;
+//         }
+        
+
+//         }
+        
+//         System.out.println(occurance);
+      
+//     }
+
+// }
+
 import java.util.Scanner;
 public class shorthand{
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter the length of the array");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the size of the array");
         int size=sc.nextInt();
         int[] arr=new int[size];
-        for(int i=0;i<size;i++){
-            System.out.print("enter the element at index = "+i +" ");
+        for(int i=0;i<arr.length;i++){
+            System.out.println("enter the element at index = "+i);
             arr[i]=sc.nextInt();
+
         }
         for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]);
-            System.out.print(" ");
+            System.out.print(arr[i]+" ");
         }
-        int total=sum(arr);
-        System.out.println("the totla sum is = "+total);
-        double avg=avg(arr);
-        System.out.println("the avg is = "+avg);
-        
+        max(arr);
+        min(arr);
+         
     }
-    public static int sum(int[] arr){
-        int sum=0;
-        for(int i=0;i<arr.length;i++){
-            sum +=arr[i];
+    public static void max(int[] arr){
+        int max=arr[0];
+         for(int i=0;i<arr.length;i++){
+            if(arr[0]<arr[i]){
+                max=arr[i];
+            }
+
         }
-        return sum;
+        System.out.println("the max element is "+max);
+
     }
-    public static double avg(int[] arr){
-       
-        double avg=(double) sum(arr)/arr.length;
-        return avg;
+    public static void min(int[] arr){
+        int min=arr[0];
+         for(int i=0;i<arr.length;i++){
+            if(arr[0]>arr[i]){
+                min=arr[i];
+            }
+
+        }
+        System.out.println("the max element is "+min);
+
     }
-    
+   
 }
